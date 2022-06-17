5ea38c3e01fd77047d2a5666c4fe79c5f0f52303
dtwnik <72192760+dtwnik@users.noreply.github.com>
2022-06-17 15:40:20 +0600

    fifth

--- server/settings.py
+++ server/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
